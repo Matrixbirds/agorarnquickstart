d5b66647985225e75fa4efe2a4a61dfdbba656f0
matrixbirds <yooobuntu@gmail.com>
2019-09-11 20:09:31 +0800

    upgrade react-native to 0.60.5

---
+++ src/settings.js
@@ -0,0 +1,9 @@
+import { DefaultTheme } from 'react-native-paper';
+
+export const theme = {
+  ...DefaultTheme
+}
+
+export const APPID = 'Your Agora.io APPID';
+
+export const title = 'Agora ReactNative Quick Start';
